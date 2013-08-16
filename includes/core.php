279fe03383cfe19286a49783b5360233f2eafc8c
Michael Beckwith <tw2113@gmail.com>
2013-08-16 12:40:06 -0500

    update translations, rename pot file, fix admin notification email spacing

--- includes/core.php
+++ includes/core.php
@@ -218,7 +218,7 @@ function wds_bp_registration_options_bp_core_activate_account($user_id){
 			$user = get_userdata( $user_id );
 			$user_name = $user->user_login;
 			$user_email = $user->user_email;
-			$mod_email = $user_name . ' (' . $user_email . ')' . __( 'would like to become a member of your website, to accept or reject their request please go to', 'bp-registration-options') . admin_url( '/admin.php?page=bp_registration_options_member_requests' );
+			$mod_email = $user_name . ' ( ' . $user_email . ' ) ' . __( 'would like to become a member of your website, to accept or reject their request please go to ', 'bp-registration-options') . admin_url( '/admin.php?page=bp_registration_options_member_requests' );
 			$admin_email = get_bloginfo( 'admin_email' );
 			wp_mail( $admin_email, __( 'New Member Request', 'bp-registration-options' ), $mod_email );
 		}
