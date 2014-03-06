8a75a2c22cb5646011e9bfc9be2c928564e07573
Michael Beckwith <tw2113@gmail.com>
2014-03-06 00:45:25 -0600

    so much cleanup!

--- includes/core.php
+++ includes/core.php
@@ -6,97 +6,14 @@
  */
 
 /**
- * set $bp_moderate & $bprwg_privacy_network globals and filter off bp buttons
- */
-function wds_bp_registration_options_core_init(){
-	global $bp_moderate, $bprwg_privacy_network, $wpdb, $bp, $user_ID, $blog_id;
-	if ( !is_admin() ) {
-		if ( is_multisite() ) {
-			$blogid = $blog_id;
-			switch_to_blog(1);
-		}
-		$bprwg_privacy_network = get_option('bprwg_privacy_network');
-		$bp_moderate = get_option('bprwg_moderate');
-		if ( is_multisite() ) {
-			switch_to_blog($blogid);
-		}
-		//non approved members and non logged in members can not view any buddypress pages
-		if ( $bprwg_privacy_network == 1 ) {
-			//redirect non logged in users to registration page, if register page is not set then kill it
-			if ( $bp->current_component && $user_ID == 0 && $bp->current_component != 'register' && $bp->current_component != 'activate' ) {
-				if ( $bp->pages->register->slug ) {
-					wp_redirect( site_url() . '/' . $bp->pages->register->slug );
-					exit();
-				} else {
-					exit();
-				}
-			//if logged in and not approved then redirect to their profile page
-			} elseif ( $bp->current_component && $user_ID > 0 && ( $bp->displayed_user->userdata == '' || $bp->displayed_user->userdata != '' && $bp->displayed_user->id != $user_ID ) ) {
-				$user = get_userdata( $user_ID );
-				if ( 69 == $user->user_status ) {
-					wp_redirect( $bp->loggedin_user->domain );
-					exit();
-				}
-			}
-		}
-		//non approved members can still view bp pages
-		if ( $bp_moderate == 1 && $user_ID > 0 ) {
-			$user = get_userdata( $user_ID );
-			if ( 69 == $user->user_status ) {
-				//hide friend buttons
-				add_filter( 'bp_get_add_friend_button', '__return_false' );
-				add_filter( 'bp_get_send_public_message_button', '__return_false' );
-				add_filter( 'bp_get_send_message_button', '__return_false' );
-				//hide group buttons
-				add_filter( 'bp_user_can_create_groups', '__return_false' );
-				add_filter( 'bp_get_group_join_button', '__return_false' );
-				//hide activity comment buttons
-				add_filter( 'bp_activity_can_comment_reply', '__return_false' );
-				add_filter( 'bp_activity_can_comment', '__return_false' );
-				add_filter( 'bp_acomment_name', '__return_false' );
-				//redirect messages page back to profile (dont want blocked members contacting other members)
-				add_filter( 'bp_get_options_nav_invite', '__return_false' );
-				add_filter( 'bp_get_options_nav_compose', '__return_false' );
-				if ( $bp->current_component == 'messages' ) {
-					wp_redirect( $bp->loggedin_user->domain );
-					exit();
-				}
-			//set global to false
-			} else {
-				$bp_moderate = false;
-			}
-		}
-	}
-}
-add_action( 'init', 'wds_bp_registration_options_core_init' );
-
-/**
- * Hide any activity created by blocked user (if they somehow get around hidden form)
- */
-function wds_bp_registration_options_bp_actions(){
-	global $wpdb, $user_ID, $bp_moderate, $bp;
-	if ( $bp_moderate ) {
-		$sql = 'UPDATE ' . $wpdb->base_prefix . 'bp_activity SET hide_sitewide = 1 WHERE user_id = %d';
-		$wpdb->query( $wpdb->prepare( $sql, $user_ID ) );
-	}
-}
-add_action( 'bp_actions', 'wds_bp_registration_options_bp_actions', 50 );
-
-/**
  * Show a custom message on the activation page and on users profile header.
  */
 function wds_bp_registration_options_bp_after_activate_content(){
-	global $bp_moderate, $user_ID, $blog_id;
-	if ( is_multisite() ) {
-		$blogid = $blog_id;
-		switch_to_blog(1);
-	}
-	if ( $bp_moderate && isset( $_GET['key'] ) || $bp_moderate && $user_ID > 0 ) {
+	global $user_ID;
+
+	if ( isset( $_GET['key'] ) || $user_ID > 0 ) {
 		$activate_message = stripslashes( get_option( 'bprwg_activate_message' ) );
 		echo '<div id="message" class="error"><p>' . $activate_message . '</p></div>';
-	}
-	if ( is_multisite() ) {
-		switch_to_blog( $blogid );
 	}
 }
 add_filter( 'bp_after_activate_content', 'wds_bp_registration_options_bp_after_activate_content' );
@@ -155,6 +72,8 @@ function wds_bp_registration_options_bp_core_activate_account($user_id){
 	}
 }
 add_action( 'bp_core_activate_account', 'wds_bp_registration_options_bp_core_activate_account');
+
+
 
 /**
  * Hide members, who haven't been approved yet, on the frontend listings.
