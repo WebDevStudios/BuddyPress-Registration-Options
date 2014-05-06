32910bb348d4f9f0fbee07f5da21b535c99e566f
Michael Beckwith <tw2113@gmail.com>
2014-05-05 21:05:54 -0500

    update name, url, description, and author meta data

--- loader.php
+++ loader.php
@@ -1,11 +1,11 @@
 <?php
 /*
-Plugin Name: BP-Registration-Options
-Plugin URI: http://wordpress.org/extend/plugins/bp-registration-options/
-Description: BuddyPress plugin that allows for new member moderation, if moderation is switched on any new members will be blocked from interacting with any buddypress elements (except editing their own profile and uploading their avatar) and will not be listed in any directory until an admin approves or denies their account.
+Plugin Name: BP Registration Options
+Plugin URI: http://wordpress.org/plugins/bp-registration-options/
+Description: This BuddyPress extension allows you to enable user moderation for new members, as well as help create a private network for your users. If moderation is enabled, any new members will be denied access to your BuddyPress and bbPress areas on your site, with the exception of their own user profile. They will be allowed to edit and configure that much. They will also not be listed in the members lists on the frontend until approved. Custom messages are available so you can tailor them to the tone of your website and community. When an admin approves or denies a user, email notifications will be sent to let them know of the decision.
 Version: 4.2
-Author: Brian Messenlehner of WebDevStudios & Jibbius
-Author URI: http://webdevstudios.com/about/brian-messenlehner/
+Author: Brian Messenlehner and Michael Beckwith of WebDevStudios, Jibbius
+Author URI: http://webdevstudios.com/about/
 Licence: GPLv3
 Text Domain: bp-registration-options
 */
