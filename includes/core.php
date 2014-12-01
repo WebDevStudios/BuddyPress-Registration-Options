4825ca975b4eb0fb41c672b31bbb643bbe5b1167
Michael Beckwith <tw2113@gmail.com>
2014-12-01 00:17:38 -0600

    cast to bool for more assurance

--- includes/core.php
+++ includes/core.php
@@ -184,7 +184,7 @@ add_action( 'bp_ready', 'bp_registration_hide_ui' );
 function bp_registration_deny_access() {
 
 	$user = new WP_User( get_current_user_id() );
-	$moderate = get_option( 'bprwg_privacy_network' );
+	$moderate = (bool) get_option( 'bprwg_privacy_network' );
 
 	if ( bp_registration_buddypress_allowed_areas() || bp_registration_bbpress_allowed_areas() || ! $moderate ) {
 		return;
