acbcb6794a50e03ea3943b45e1330d7d097cb58f
Michael Beckwith <tw2113@gmail.com>
2015-10-31 13:55:07 -0500

    initial test file to get the directory in vcs

---
+++ tests/phpunit/tests/bpro-loader-test.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * BP Registration Options Tests
+ *
+ * @package BP-Registration-Options-Tests
+ */
+
+/**
+ * Tests our functions found in the loader.php file
+ *
+ * @since 4.3.0
+ */
+class bpro_loader_test extends WP_UnitTestCase {
+
+}
