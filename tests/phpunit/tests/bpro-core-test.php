cbe43ea243dfbeff7a7dc73c6106acc3042ff8e4
Michael Beckwith <tw2113@gmail.com>
2015-11-01 14:35:50 -0600

    add some moderation status tests

--- tests/phpunit/tests/bpro-core-test.php
+++ tests/phpunit/tests/bpro-core-test.php
@@ -12,16 +12,43 @@
  */
 class bpro_core_test extends WP_UnitTestCase {
 
+	/**
+	 * Tests the return values from moderation status.
+	 *
+	 * Based on boolean return value compared to text in user meta.
+	 */
 	public function test_get_moderation_status() {
-		/*
-		 * Create user, set moderation status, check returned value.
-		 */
+		$u1 = $this->factory->user->create();
+		$return = bp_registration_set_moderation_status( $u1 );
+		$this->assertTrue( bp_registration_get_moderation_status( $u1 ) );
+		$return = bp_registration_set_moderation_status( $u1, 'false' );
+		$this->assertFalse( bp_registration_get_moderation_status( $u1 ) );
 	}
 
+	/**
+	 * Tests the setting of moderation status.
+	 *
+	 * Return value is the integer of the user meta row created if successful,
+	 * thus why we're checking for is_int for legit users.
+	 */
 	public function test_set_moderation_status() {
-		/*
-		 * Create user, set moderation status, check status of bp_registration_get_moderation_status().
-		 */
+		$u1 = $this->factory->user->create();
+
+		// Non existing user.
+		$invalid_user = bp_registration_set_moderation_status( 42 );
+		$this->assertFalse( $invalid_user );
+
+		// No user meta set yet.
+		$empty = bp_registration_get_moderation_status( $u1 );
+		$this->assertFalse( $empty );
+
+		// Setting to moderated.
+		$set1 = bp_registration_set_moderation_status( $u1 );
+		$this->assertTrue( is_int( $set1 ) );
+
+		// Setting to not moderated.
+		$set2 = bp_registration_set_moderation_status( $u1, 'false' );
+		$this->assertTrue( is_int( $set2 ) );
 	}
 
 	public function test_admin_bar_additions() {
